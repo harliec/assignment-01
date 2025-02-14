901973ff69a2131a461eafe344e316dd752f0abb
harliec <harliec@aol.com>
2025-02-13 20:20:37 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,182 @@
+//STEP 1 to Camel Case  Notation
+/*
+let someMonth
+function theMonth()
+let currentMonth
+let summerMonth
+let myLibraryFunction
+*/
+
+//STEP 2  examples of literal expressions
+/*
+  A numeric literal expression
+const width = 3
+  B string literal expression
+const firstName = 'John'
+  C boolean literal expression
+const isTrue = true
+  D null literal expression
+const myValue = null
+*/
+
+//STEP 3 complex / variable expressions
+/*
+let person = 'Charlie Carroll' ,
+    age = 30,
+    phone = '619-619-6199'
+    console.log(person + "is age" + age + "his phone number is" + phone)
+*/
+
+//STEP 4     9 VARIABLES
+/*
+let firstName
+let lastName
+let address
+let city
+let state   
+let zipCode
+let age
+let referalSource
+let mayWeContactYou
+*/
+
+//STEP 5     demonstrate declaring and assigning to variables
+/*
+firstName = 'John'
+firstName = 'John',lastName = 'Brown'
+firstName = 'John'
+lastName = 'Brown'
+address = '1601 Broadway'
+*/
+
+//STEP 6     acreate var 
+/*
+let rabbit = 77 + "bugs"
+console.log(rabbit)
+answer in browser console 77bugs
+*/
+//STEP 7 Two vars
+/*  1st one = add boolean and string
+ 2nd one = add number and boolean
+/* 1st one
+
+let str = true + 'hello'
+console.log(str)
+results = truehello
+ 
+2nd one
+let str = 88 + true
+console.log(str)
+results = 89
+*/  
+
+//STEP 8
+/*
+let stng = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I’m not sure about the former."'
+/*
+step 7 needs a backslash before the single quote in I'm
+*/
+
+//STEP 9 produce a) NAN in console ,b) undefined in console
+/*let num = "abc" * 2
+console.log(num) 
+NAN
+
+let num = 
+console.log(num)
+Results = undefined
+*/
+
+//STEP 10 typeof operator
+
+/*console.log(typeof "kat")
+//Results = string
+console.log(typeof 4)   
+//Results = number
+console.log(typeof true)
+//Results = boolean
+console.log(typeof Object)
+console.log(typeof "undefined")
+*/
+
+//STEP 11 
+/*
+aert('Hello ' + 'Charles Carroll ' + 'welcome to the JavaScript class')
+*/
+
+//STEP 12
+/*
+let name = 'Charles Carroll'
+alert("Hello " + name + ", welcome to the jclass")
+*/
+
+//STEP 13
+/*
+let name = 'Charles Carroll'
+let course = 'JavaScript'
+alert('Hello ' + name + ', welcome to the ' + course + ' class')
+*/
+
+//STEP 14
+/*
+let name = 'Charles Carroll'
+let course = 'JavaScript'
+alert('Hello ' + name + ',/n welcome to the ' + course + ' class')
+*/
+
+//STEP 15
+/*
+let name = prompt('What is your name?')
+let course = 'JavaScript'
+alert('Hello ' + name + ',/n welcome to the ' + course + ' class')
+*/
+
+//STEP 16
+/*
+let name = prompt('What is your name?')
+let course = prompt('What course are you taking?')
+alert('Hello ' + name + ',/n welcome to the ' + course + ' class')
+*/
+
+//STEP 17
+/*
+x = 10
+y = 20
+sum = x + y
+console.log(sum) 
+*/
+
+//STEP 18
+/*
+x = 20
+console.log(x + 20)
+*/
+
+//STEP 19
+/*
+x = 20
+console.log(x * 5)
+
+*/
+
+//STEP 20
+/*
+x = 2 
+y = 1
+console.log(x = x / y)
+*/
+
+//STEP 21
+/*
+m = 22
+n = 19
+console.log(m == n)
+Results = false
+*/
+
+//STEP 22
+/*
+console.log('abc' === 'bca')
+Results = false
+*/  
+
